eaa9141c51f07ed97fd407a72f43f423d89ce832
mly-zju <malingyang@foxmail.com>
2018-04-13 16:38:01 +0800

    remove fecs depedency

--- index.js
+++ index.js
@@ -3,6 +3,15 @@
  * @file cli工具入口
  * @author malingyang@foxmail.com
  */
+// 检测全局fecs是否安装
+try {
+    require('fecs');
+}
+catch (err) {
+    console.log('请首先全局安装fecs，否则无法进行代码规范校验!');
+    process.exit(1);
+}
+
 let config = process.argv[2];
 
 if (config === '-p' || config === '--progressive') {
