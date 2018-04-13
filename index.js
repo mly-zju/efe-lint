a2882b293d5463c840002e7ef1533e2b9bedf46a
mly-zju <malingyang@foxmail.com>
2018-04-13 16:53:10 +0800

    check global fecs

--- index.js
+++ index.js
@@ -3,22 +3,26 @@
  * @file cli工具入口
  * @author malingyang@foxmail.com
  */
-// 检测全局fecs是否安装
-try {
-    require('fecs');
-}
-catch (err) {
-    console.log('请首先全局安装fecs，否则无法进行代码规范校验!');
-    process.exit(1);
-}
+let exec = require('child_process').exec;
 
-let config = process.argv[2];
+exec('npm link fecs', () => {
+    try {
+        require('fecs');
+    }
+    catch (err) {
+        console.log('请首先全局安装fecs，否则无法进行代码规范校验!');
+        process.exit(1);
+    }
 
-if (config === '-p' || config === '--progressive') {
-    require('./src/progressive');
-}
-else {
-    let path = config || './';
-    let f = require('./src/normal');
-    f(path);
-}
+    let config = process.argv[2];
+
+    if (config === '-p' || config === '--progressive') {
+        require('./src/progressive');
+    }
+    else {
+        let path = config || './';
+        let f = require('./src/normal');
+        f(path);
+    }
+});
+
